519ed30746ab6aa64825b46671fbde48014e4153
Dmitry Ryzhov <softwaretester6666@gmail.com>
2025-05-01 17:43:14 +0300

    added PracticeFromTests

---
+++ src/test/java/tests/PracticeFromTests.java
@@ -0,0 +1,28 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class PracticeFromTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+        Configuration.timeout = 5000; // default 4000
+    }
+
+    @Test
+void fillFormsTests () {
+        open("/automation-practice-form");
+    }
+}
+
+
