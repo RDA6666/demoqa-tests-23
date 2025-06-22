f947d7ecb653876801363b40d9e542f1607a21e3
Dmitry Ryzhov <softwaretester6666@gmail.com>
2025-06-22 12:29:52 +0300

    updated again PracticeFromTests

--- src/test/java/tests/NegativeRegistrationTests.java
+++ src/test/java/tests/NegativeRegistrationTests.java
@@ -26,15 +26,13 @@ public class NegativeRegistrationTests {
             executeJavaScript("$('footer').remove()");
             $("#firstName").setValue("John");
             $("#lastName").setValue("Doe");
-            $("#userEmail").setValue("1111");
             $("#genterWrapper").$(byText("Male")).click();
-            $("#userNumber").setValue("89841658948");
+            $("#userNumber").setValue("a");
             $("#submit").click();
 
-            $x("//td[contains(text(),'Student Name')]/../td[2]").shouldHave(text("John Doe"));
-            $x("//td[contains(text(),'Student Email')]/../td[2]").shouldHave(text("1111"));
-            $x("//td[contains(text(),'Gender')]/../td[2]").shouldHave(text("Male"));
-            $x("//td[contains(text(),'Mobile')]/../td[2]").shouldHave(text("89841658948"));
+            $("#firstName").shouldHave(text("John Doe"));
+            $("#lastName").shouldHave(text("Male"));
+            $("#userNumber").shouldHave(text("a"));
 
         }
 }
