348f873108572db9f5da9ddf5232c5c348ea2ad6
Dmitry Ryzhov <softwaretester6666@gmail.com>
2025-06-22 13:13:09 +0300

    updated again PracticeFromTests

--- src/test/java/tests/NegativeRegistrationTests.java
+++ src/test/java/tests/NegativeRegistrationTests.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
@@ -18,23 +19,34 @@ public class NegativeRegistrationTests {
         Configuration.timeout = 5000; // default 4000
     }
 
-        @Test
-        void negativeRegistrationTest () {
+    @Test
+    void negativeRegistrationTest() {
+        open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+        $("#firstName").setValue("John");
+        $("#lastName").setValue("Doe");
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#userNumber").setValue("a");
+        $("#submit").click();
 
-            open("/automation-practice-form");
-            executeJavaScript("$('#fixedban').remove()");
-            executeJavaScript("$('footer').remove()");
-            $("#firstName").setValue("John");
-            $("#lastName").setValue("Doe");
-            $("#genterWrapper").$(byText("Male")).click();
-            $("#userNumber").setValue("a");
-            $("#submit").click();
+        $(".modal-dialog").shouldNot(appear);
+        $(".row").shouldNotHave(text("Thanks for submitting the form"));
+    }
 
-            $(".row").shouldNotHave(text("Thanks for submitting the form"));
-        }
+    @Test
+    void minimumFieldsRegistrationTest() {
+        open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+        $("#firstName").setValue("John");
+        $("#lastName").setValue("Doe");
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#userNumber").setValue("1234567890");
+        $("#submit").click();
 
-        @Test
-        void minimumFieldsRegistrationTest () {
-
-        }    }
+        $(".modal-dialog").should(appear);
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("John"), text("Doe"), text("1234567890"));
+    }
 }
