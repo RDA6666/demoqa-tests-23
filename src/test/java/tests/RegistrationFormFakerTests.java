b582e55e0bc6f0982edc0ddeeb248a6e400cb0c7
Dmitry Ryzhov <softwaretester6666@gmail.com>
2025-06-25 20:30:37 +0300

    updated RegistrationFormFakerTests.java

--- src/test/java/tests/RegistrationFormFakerTests.java
+++ src/test/java/tests/RegistrationFormFakerTests.java
@@ -1,4 +1,62 @@
 package tests;
 
-public class RegistrationFormFakerTests {
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class RegistrationFormFakerTests extends TestBase {
+
+    @Test
+    void successfulRegistrationTest() {
+
+        Faker faker = new Faker();                                                         //random test data generation
+        String firstName = faker.name().firstName(),
+                lastName = faker.name().lastName(),
+                userEmail = faker.internet().emailAddress(),
+                userPhoneNumber = faker.phoneNumber().cellPhone(),
+                userCurrentAddress = faker.address().fullAddress();
+
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
+        $("#firstName").setValue(firstName);                                                    //text fields
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(userEmail);
+
+        $("#genterWrapper").$(byText("Other")).click();                             //radio button
+
+        $("#userNumber").setValue(userPhoneNumber);                                              //text field
+
+        $("#dateOfBirthInput").click();                                                            //calendar
+        $(".react-datepicker__month-select").selectOption("July");
+        $(".react-datepicker__year-select").selectOption("2008");
+        $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
+
+        $("#subjectsInput").setValue("Math").pressEnter();                         //text field-dropdown list
+
+        $("#hobbiesWrapper").$(byText("Sports")).click();                               //checkbox
+
+        $("#uploadPicture").uploadFromClasspath("images/Me.jpg");                       //file upload
+
+        $("#currentAddress").setValue(userCurrentAddress);                                         //textarea
+
+        $("#state").click();                                                     //text fields-dropdown lists
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+
+        $("#submit").click();                                                                        //button
+
+        $(".modal-dialog").should(appear);                                                       //assertions
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text(firstName),
+                text(lastName), text(userEmail), text(userPhoneNumber), text(userCurrentAddress));
+    }
 }
