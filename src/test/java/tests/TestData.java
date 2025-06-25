ac9cf31f06628f00dcf3bd1d5bf13b1fe4988645
Dmitry Ryzhov <softwaretester6666@gmail.com>
2025-06-25 22:08:31 +0300

    updated TestData.java

--- src/test/java/tests/TestData.java
+++ src/test/java/tests/TestData.java
@@ -1,7 +1,8 @@
 package tests;
 
 public class TestData {
-    public static String firstName = "Dmitry",
+    public static String
+            firstName = "Dmitry",
             lastName = "Ryzhov",
             userEmail = "dmitryryzhov@mail.com",
             userPhoneNumber = "1234567890",
