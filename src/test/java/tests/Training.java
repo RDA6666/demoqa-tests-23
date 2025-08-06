a23bf896ebd25d138cbd90226a0597ec667a47c6
Dmitry Ryzhov <softwaretester6666@gmail.com>
2025-08-06 19:40:15 +0300

    added tags

---
+++ src/test/java/tests/Training.java
@@ -0,0 +1,10 @@
+package tests;
+
+
+public class Training {
+    public static void main(String[] args) {
+        System.out.println("Привет!");
+        System.out.println("Привет!");
+        System.out.println("Привет!");
+    }
+}
