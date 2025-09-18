eb2a0dff7b1825e5d4aa6cd50029b76c4ca0ba68
Dmitry Ryzhov <softwaretester6666@gmail.com>
2025-09-18 16:10:05 +0300

    added allure and jenkins 1

---
+++ src/test/java/package1/Cat.java
@@ -0,0 +1,9 @@
+package package1;
+
+public class Cat {
+    public String name;
+
+    public Cat(String catName) {
+        name = catName;
+    }
+}
---
+++ src/test/java/package2/Main.java
@@ -0,0 +1,15 @@
+package package2;
+
+import package1.Cat;
+
+public class Main {
+    public static void main(String[] args) {
+        Cat cat = new Cat("Tom");
+        System.out.println(cat.name);
+    }
+}
+
+
+
+
+
